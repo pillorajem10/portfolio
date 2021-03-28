4b547a60d12c2702f4e61553ad6c3dde8c6116b6
Your Name <pillorajem10@gmail.com>
2021-03-28 13:29:57 +0800

    fixed typo

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -15,7 +15,7 @@ const Page = () => {
         <div data-aos="zoom-in" className= "about-container">
           <p className="container-title">About Me</p>
           <p>
-            I am a 20 yr old freshman college of AMA University, I study full stack well development for it is what I enjoy doing. I aspire to be a web developer in the near future so as to achieve my ever-growing passion for web development, it would also allow me to work in a corporation of my choice.
+            I am a 20 yr old freshman college of AMA University, I study full-stack web development for it is what I enjoy doing. I aspire to be a web developer in the near future so as to achieve my ever-growing passion for web development, it would also allow me to work in a corporation of my choice.
           </p>
         </div>
       </div>
